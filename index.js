9c9e2cee050e9952d53b1b10b9a53f594d6b81df
jjkusowski <jjkusowski@yahoo.com>
2017-10-09 16:09:02 -0700

    Player 1 and 2 message on index. Correctly takes 2 clicks and moves to fight.html

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 'use strict';
 var fightData = [];
 var characterMenu = document.getElementById('roster');
+var message = document.getElementById('user-prompt');
 
 localStorage.setItem('fightData', JSON.stringify(fightData));
 
 characterMenu.addEventListener('click', characterHandler);
 
 function characterHandler(event){
-  if (fightData.length < 2) {
+  if (fightData.length < 1) {
     fightData.push(event.target.alt);
+    message.textContent = 'PLAYER 2 SELECT YOUR CHARACTER';
   } else {
+    fightData.push(event.target.alt);
     localStorage.setItem('fightData', JSON.stringify(fightData));
     window.location.href = 'fight.html';
   }
