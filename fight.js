e0e909dd2112d02f5d5c79a43dc00e43e97fd4b5
jjkusowski <jjkusowski@yahoo.com>
2017-10-10 10:03:48 -0700

    fight.js loads local storage and checks for not enough info.

--- fight.js
+++ fight.js
@@ -1,10 +1,31 @@
+'use strict';
+var fightData = [];
+fightData = JSON.parse(localStorage.getItem('fightData'));
+
 //load localStorage
+if (fightData === null || fightData.length < 2) {
+  alert('You have arrived here in error.  Return to character select screen!');
+  window.location.href = 'index.html';
+}
+
 //Character construction function
+
+
 //all cats being instanced
+
+
 //random function that takes in min and max
+
+
 //function dedicated to the heal
+
+
 //function dedicated to the attack
+
+
 //listener to listen for which move to call
+
+
 //function to to win/lose screen
 //function to hide other player's buttons
 //once victor has won, enter in username and redirect that information to leaderboard
