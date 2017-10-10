726f5c5719dde44fdb29632dc81c45b5f0d56334
William Johnson <williamgjohnson@live.com>
2017-10-09 17:19:56 -0700

    psuedocode

---
+++ fight.js
@@ -0,0 +1,11 @@
+//load localStorage
+//Character construction function
+//all cats being instanced
+//random function that takes in min and max
+//function dedicated to the heal
+//function dedicated to the attack
+//listener to listen for which move to call
+//function to to win/lose screen
+//function to hide other player's buttons
+//once victor has won, enter in username and redirect that information to leaderboard
+//figure out how to use sort
